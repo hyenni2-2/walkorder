424829eba5b1a3a4d55d2293749c74b2c84c35c2
HyeinChoi <hi.choi1031@gmail.com>
2021-03-27 16:34:25 +0900

    210327 BootStrap 및 CSS, DB 연결

--- map/src/main/java/com/wo/map/indexController.java
+++ map/src/main/java/com/wo/map/indexController.java
@@ -1,15 +1,20 @@
 package com.wo.map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class indexController {
 	
-	@GetMapping("/")
+	@RequestMapping(value="/index", method=RequestMethod.GET)
 	public String getIndex() {
-		return "Index Page";
+		return "index";
+	}
+	
+	@RequestMapping(value="/map", method=RequestMethod.GET)
+	public String getMap() {
+		return "map/map";
 	}
 
 }
