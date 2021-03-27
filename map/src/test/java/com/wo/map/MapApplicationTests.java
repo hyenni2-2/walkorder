3ffac3856dc7e3691c0f0978492cf3597b830da1
HyeinChoi <hi.choi1031@gmail.com>
2021-03-27 12:19:01 +0900

    210327 first

---
+++ map/src/test/java/com/wo/map/MapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
